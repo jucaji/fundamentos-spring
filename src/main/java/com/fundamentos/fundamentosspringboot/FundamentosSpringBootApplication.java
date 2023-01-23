49803131dad097d1320ed3b6afc4c4fdc20ed8e2
jucaji <jcjvirtual@gmail.com>
2023-01-23 12:31:59 -0500

    primer commit

---
+++ src/main/java/com/fundamentos/fundamentosspringboot/FundamentosSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentos.fundamentosspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentosSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FundamentosSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fundamentos/fundamentosspringboot/FundamentosSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentos.fundamentosspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentosSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
